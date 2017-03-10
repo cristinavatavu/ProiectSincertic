83efb07715b3a20dd40751d495ebc7c2b501e146
cristinavatavu <cristinavatavu@gmail.com>
2017-03-10 23:26:20 +0200

    Add files via upload

---
+++ sketch/Defines.h
@@ -0,0 +1,40 @@
+/*
+ * Defines.h
+ *
+ * Created: 3/9/2017 12:14:14 AM
+ *  Author: Cristina
+ */ 
+
+
+#ifndef DEFINES_H_
+#define DEFINES_H_
+
+
+/*
+#define LCD_LEFT_COL 0
+#define LCD_RIGHT_COL 8
+
+#define LCD_FIRST_ROW 0
+#define LCD_SECOND_ROW 1*/
+
+
+
+#endif /* DEFINES_H_ */
+
+
+
+/*
+ The circuit:
+ * LCD RS pin to digital pin 12
+ * LCD Enable pin to digital pin 11
+ * LCD D4 pin to digital pin 5
+ * LCD D5 pin to digital pin 4
+ * LCD D6 pin to digital pin 3
+ * LCD D7 pin to digital pin 2
+ * LCD R/W pin to ground
+ * LCD VSS pin to ground
+ * LCD VCC pin to 5V
+ * V0 pin to ground
+ * K pin to ground
+ * A pin to 5V with a resistor (680 ohms)
+ */
---
+++ sketch/MainProgram.cpp
@@ -0,0 +1,50 @@
+﻿
+//INCLUDES
+#include <avr/io.h>
+#include <Arduino.h>
+#include <LiquidCrystal.h>
+#include <string.h>
+#include <stdlib.h>
+#include "Defines.h"
+
+
+
+//GLOBAL VARIABLES
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+
+
+//PROTOTYPES
+void setup();
+void loop();
+int Display(int, int, int);
+
+
+
+//FUNCTIONS
+int main(void)
+{
+	setup();
+	
+	lcd.setCursor(0, 0);  //coloana(0-15, 8 e mijlocul), rand (0-1)
+	lcd.print("hello world");
+	
+	while (1)
+	{
+		loop();
+	}
+}
+
+
+
+
+void setup ()
+{
+	lcd.begin(16, 2); //nr columns, nr rows
+}
+
+
+void loop()
+{
+	
+}
